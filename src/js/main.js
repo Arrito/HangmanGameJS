8b6dc06f172e979f0b1d084ff9411e2d7d3ab29e
Arrito <arianalovelace43@gmail.com>
2024-11-29 16:52:37 +0300

    feat: add start screen, dark mode, main btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import '../css/style.css'
+import {darkModeHandle} from './utils'
+
+darkModeHandle()
+
+const clickStartGame = document.getElementById('startGame')
+clickStartGame.addEventListener('click', ()=>{console.log('click');
+})
+
+
+
