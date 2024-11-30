34ef49850aee2d6c85832ffb1f2a16a631ad35ce
Arrito <arianalovelace43@gmail.com>
2024-11-30 13:38:59 +0300

    feat: add  create a function to handle the entered letter fix: keyboard bug, Set up a ban on pressing the button again

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,8 @@ import { WORDS, KEYBOARD_LETTERS } from "./const";
 const gameDiv = document.getElementById("game");
 const logoH1 = document.getElementById("logo-text");
 
+let triesLeft;
+
 const createPlaceholderHTML = () => {
   const word = sessionStorage.getItem("word");
 
@@ -38,7 +40,30 @@ const createHangmanImg = () => {
   return image;
 };
 
+const checkLetter = (letter) => {
+  const word = sessionStorage.getItem("word");
+  let inputLetter = letter.toLowerCase();
+  if (!word.includes(inputLetter)) {
+    const triesCounter = document.getElementById("tries-left");
+    triesLeft -= 1;
+    triesCounter.innerText = triesLeft;
+
+    const hangmanImg = document.getElementById("hangman-image");
+    hangmanImg.src = `images/hg-${10 - triesLeft}.png`;
+  } else {
+    const wordArray = Array.from(word);
+    wordArray.forEach((currentLetter, i) => {
+      if (currentLetter === inputLetter) {
+        document.getElementById(`letter_${i}`).innerText =
+          inputLetter.toUpperCase();
+      }
+    });
+  }
+};
+
 export const startGame = () => {
+  triesLeft = 10;
+
   logoH1.classList.add("logo-textSM");
 
   const randomIndex = Math.floor(Math.random() * WORDS.length);
@@ -47,11 +72,14 @@ export const startGame = () => {
 
   gameDiv.innerHTML = createPlaceholderHTML();
 
-  gameDiv.innerHTML += `<p id='tries' class='mt-2'>TRIES LEFT: <span id='tries-left' class='font-medium text-red-600'>10</span></p>`;
+  gameDiv.innerHTML += `<p id='tries' class='mt-2'>TRIES LEFT: <span id='tries-left' class='font-medium text-red-600'>${triesLeft}</span></p>`;
 
   const keyboardDiv = createKeyboard();
   keyboardDiv.addEventListener("click", (event) => {
-    console.log(event.target.id);
+    if (event.target.tagName.toUpperCase() === "BUTTON") {
+      event.target.disabled = true;
+      checkLetter(event.target.id);
+    }
   });
 
   //  gameDiv.innerHTML += createKeyboard( )
